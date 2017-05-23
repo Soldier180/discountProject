1713bcbd120a415b46bb2de9b75bcd315afd0b12
Nikolay <Soldier180@gmail.com>
2017-05-23 14:41:26 +0300

    final commit

--- src/main/java/ResultWindowController.java
+++ src/main/java/ResultWindowController.java
@@ -14,7 +14,8 @@ public class ResultWindowController {
     DecimalFormat df = new DecimalFormat("#.##");
 
     public void setPriceValues(double clothesPrice ,
-                               double shoesPrice, double accessoriesPrice){
+                               double shoesPrice,
+                               double accessoriesPrice){
         lblAccessories.setText(df.format(accessoriesPrice));
         lblClothes.setText(df.format(clothesPrice));
         lblShoes.setText(df.format(shoesPrice));
